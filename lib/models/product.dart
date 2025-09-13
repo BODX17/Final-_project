f13a02f3e138403de6a734cd0cb8aef1e147946b
BODX17 <mbedo9700@gmail.com>
2025-09-13 17:14:58 +0300

    first commit

---
+++ lib/models/product.dart
@@ -0,0 +1,46 @@
+// lib/models/product.dart
+class Product {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+
+  // حقل قابل للتعديل لتخزين الكمية
+  int quantity;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    this.quantity = 1,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: (json['id'] as num).toInt(),
+      title: (json['title'] ?? '') as String,
+      price: (json['price'] as num).toDouble(),
+      description: (json['description'] ?? '') as String,
+      category: (json['category'] ?? '') as String,
+      image: (json['image'] ?? '') as String,
+      quantity: json['quantity'] != null ? (json['quantity'] as num).toInt() : 1,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'price': price,
+      'description': description,
+      'category': category,
+      'image': image,
+      'quantity': quantity,
+    };
+  }
+}
